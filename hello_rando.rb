2e6af27e2b220b169254f8967b18b57f1ea8b8d5
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-01 14:44:29 -0500

    Random Number Generator
    Generates random numbers

---
+++ hello_rando.rb
@@ -0,0 +1,15 @@
+#Explores random number generation
+
+puts 'Ruby comes with a pretty nice random number generator. The method to get a randomly chosen number is rand'
+
+puts 'If you just call rand you get a float between 0.0 and 1.0'
+
+puts 'When calling rand always go one number above the last intiger you want to include'
+
+puts rand
+puts rand
+puts rand
+puts(rand(5))
+puts(rand(100))
+
+
