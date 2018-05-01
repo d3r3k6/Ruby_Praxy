d4bf2438034387b95d511cefb14d52a7b764addb
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-01 12:55:18 -0500

    Calc Program
    Calc program from Pine's exercsies

---
+++ pine_calc.rb
@@ -0,0 +1,15 @@
+=begin
+Write a program which tells you: 
+how many hours are in a year? 
+how many minutes are in a decade? 
+how many seconds old are you? 
+=end
+    
+#This will calculate hours in a year
+puts 24*365
+
+#This calculates minutes in a decade
+puts 60*24*365*10
+
+#This calculates how many seconds old
+puts (32*365*24*60) + (120*24*60)
