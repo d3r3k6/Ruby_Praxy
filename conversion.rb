1dd0d51feac581fec1e9137de7837bf94e77a5b6
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-01 13:35:21 -0500

    Conversion Practice
    program that uses conversions

---
+++ conversion.rb
@@ -0,0 +1,16 @@
+#puts stands for put string and it converts everything to a string
+#change a int to a string with .to_s
+#This should change to '25'
+var1 = 2
+var2 = '5'
+puts var1
+puts var2
+puts var1.to_s + var2
+
+#Can convert to int with .to_i
+puts var1
+puts var2
+puts var1 + var2.to_i
+
+#convert to float with .to_f
+puts var1.to_f
