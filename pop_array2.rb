02fa8debc2293cd5f4d297c675913f91a2134653
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-04 11:11:48 -0500

    Sorting Algorithm
    Implimented pop_array without sort method.

---
+++ pop_array2.rb
@@ -0,0 +1,15 @@
+#This program allows you to enter as many words as you like and reads them back to you in alaphabetical order
+#The task is to sort an array without using the sort method
+
+array = []
+puts "Please enter your work and press enter. Leave blank to initiate sort"
+word = gets.chomp.downcase
+
+while word != ''
+  array.push word
+  puts 'Please enter your next word.'
+  word = gets.chomp.downcase
+end
+
+sorted_array = array.sort
+puts sorted_array
