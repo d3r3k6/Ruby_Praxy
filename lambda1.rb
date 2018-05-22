e38af28c9e0a75d113eb3b957a44a6ff63b16dd1
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-21 20:16:17 -0500

    Added has example

--- lambda1.rb
+++ lambda1.rb
@@ -2,7 +2,25 @@
 
 my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
 
-# Add your code below!
+
 symbol_filter = lambda { |x| x.is_a? Symbol }
 symbols = my_array.select(&symbol_filter)
 puts symbol
+
+#Here is an example of how to do the same with a hash table. The key value are symbols
+
+crew = {
+    captain: "Picard",
+    first_officer: "Riker",
+    lt_cdr: "Data",
+    lt: "Worf",
+    ensign: "Ro",
+    counselor: "Troi",
+    chief_engineer: "LaForge",
+    doctor: "Crusher"
+  }
+  
+  first_half = lambda { |x,y| y<"M"}
+  a_to_m = crew.select(&first_half)
+  
+  puts a_to_m
