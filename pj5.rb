fd37c12b832b6fd948511d0248fbe477058f2f17
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-24 14:30:07 -0500

    Project Euler 5
    First commit for project euler 5

---
+++ pj5.rb
@@ -0,0 +1,10 @@
+=begin
+ is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?   
+=end
+
+evaluator = 1
+for x in (1..20)
+  number = 1
+  number.each do |
+end    
