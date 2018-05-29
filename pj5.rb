1337d8fcf1d76339c6941bcd89aa86d91d35fc6c
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-29 16:22:57 -0500

    Syntax Error
    Line 15 is trowing a syntax error. Needs refactor.

--- pj5.rb
+++ pj5.rb
@@ -3,7 +3,7 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?   
 =end
 
-
+#This is throwing a syntax error for line 15. Need refactor.
 range_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 number = 20
 for range_array.each do |x|
