1dcab0682180e421430c0bda248ca389d4522e1d
d3r3k6 <38481105+d3r3k6@users.noreply.github.com>
2018-05-31 10:09:44 -0500

    Refactored To Shorter
    Added a shorter refactor

--- c2fi.rb
+++ c2fi.rb
@@ -4,3 +4,10 @@ print "Please enter the Celsius value that you would like converted."
 celsius = gets.to_i
 fahrenheit = (celsius * 9/5) + 32
 print "#{celsius.to_s} degrees celsius is #{fahrenheit.to_s} degrees fahrenheit."
+
+
+#A consdierably shorter and perhaps more rubyist way to write this is as follows:
+#However, since I will be writing primarily for enterprise I feel more explicit is better
+
+print "Hello. Please enter a Celsius value: "
+print "The Fahrenheit equivalent is ", gets.to_i * 9 / 5 + 32, ".\n"
